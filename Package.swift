36007940458ddb54f0f3c44fb042b17e15da953b
Samantha Nano <sgnano21@gmail.com>
2025-04-16 11:40:57 -0700

    update min deployment target

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,9 +6,9 @@ import PackageDescription
 let package = Package(
     name: "MijickCalendarView",
     platforms: [
-        .iOS(.v14),
-        .macOS(.v11),
-        .watchOS(.v7)
+        .iOS(.v17),
+        .macOS(.v14),
+        .watchOS(.v10)
     ],
     products: [
         .library(name: "MijickCalendarView", targets: ["MijickCalendarView"])
